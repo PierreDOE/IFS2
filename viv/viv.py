00779f62f7011adec9f88bd5d231a6d35b8e910d
BatisteRiviere <batiste.riviere@univ-tlse3.fr>
2025-01-30 13:43:48 +0100

    Update viv.py

--- viv/viv.py
+++ viv/viv.py
@@ -15,23 +15,25 @@ import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 from viv.colored_messages import set_section
 
+
 class VIV(object):
     """
     """
+
     def __init__(self, par):
         """
         initialisation
         """
-        self.A = par["A"]                       # coupling parameter
-        self.M = par["mass_number"]             # Mass number
-        self.mu = par["mu"]                     # mass parameter
-        self.gamma = par["gamma"]               # fluid added damping coefficient
-        self.xi = par["xi"]                     # tension cable parameter
-        self.nu = par["nu"]                     # 0: no damping, 1 : full case
-        self.u = par["u"]                       # reduced flow velocity
+        self.A = par["A"]  # coupling parameter
+        self.M = par["mass_number"]  # Mass number
+        self.mu = par["mu"]  # mass parameter
+        self.gamma = par["gamma"]  # fluid added damping coefficient
+        self.xi = par["xi"]  # tension cable parameter
+        self.nu = par["nu"]  # 0: no damping, 1 : full case
+        self.u = par["u"]  # reduced flow velocity
         self.epsilon = par["epsilon"]
         self.beta = self.gamma / self.mu
-        self.option = par["option"]             # various options
+        self.option = par["option"]  # various options
         self.k_ref = par["k_ref"]
         self.k_lim = par["k_lim"]
         self.u_lim = par["u_lim"]
@@ -63,17 +65,18 @@ class VIV(object):
         """
         set_section("solve omega")
         self.set_k_table()
-        #self.set_u_table()
+        # self.set_u_table()
         self.omega = []
         i = 0
         for k in self.k_table:
-            self.omega.append(sorted(np.roots(self.set_polynomial_coefs(k,self.u)), reverse=True))
+            self.omega.append(sorted(np.roots(self.set_polynomial_coefs(k, self.u)), reverse=True))
             if print_ != 'all':
                 cond = i % 5 == 0
             else:
                 cond = True
             if info and cond:
-                print(f'k = {k:.2f}\tdiscriminant = {self.discriminant(k):.2f}\t\tomega = {sorted(np.roots(self.set_polynomial_coefs(k, self.u)), reverse=True)}')
+                print(
+                    f'k = {k:.2f}\tdiscriminant = {self.discriminant(k):.2f}\t\tomega = {sorted(np.roots(self.set_polynomial_coefs(k, self.u)), reverse=True)}')
             i += 1
 
     def solve_omega_for_u_range(self, info=False, print_='else'):
@@ -81,19 +84,20 @@ class VIV(object):
         Calcule omega pour une plage de valeurs de u.
         """
         set_section("solve omega")
-        #self.set_k_table()
+        # self.set_k_table()
         self.set_u_table()
-        #self.set_u_table()
+        # self.set_u_table()
         self.omega = []
         i = 0
         for u in self.u_table:
-            self.omega.append(sorted(np.roots(self.set_polynomial_coefs(self.k_ref,u)), reverse=True))
+            self.omega.append(sorted(np.roots(self.set_polynomial_coefs(self.k_ref, u)), reverse=True))
             if print_ != 'all':
                 cond = i % 5 == 0
             else:
                 cond = True
             if info and cond:
-                print(f'k = {k:.2f}\tdiscriminant = {self.discriminant(k):.2f}\t\tomega = {sorted(np.roots(self.set_polynomial_coefs(k, self.u)), reverse=True)}')
+                print(
+                    f'u = {u:.2f}\tdiscriminant = {self.discriminant(u):.2f}\t\tomega = {sorted(np.roots(self.set_polynomial_coefs(self.k, u)), reverse=True)}')
             i += 1
 
     def solve_gain(self):
@@ -102,19 +106,19 @@ class VIV(object):
         return gain and phase list for all omega complex variables
         """
         for omg, k in zip(np.array(self.omega), self.k_table):
-            gain = self.A * omg ** 2 / (omg ** 2 - self.u ** 2)
+            gain = self.A * omg ** 2 / (omg ** 2 - self.u ** 2 + 1j * self.nu * self.epsilon * omg * self.u)
             self.gain.append(gain)
             self.phase.append(np.angle(-gain, deg=False))
-    
+
     def solve_gain_u(self):
         """
         G(omega) = \hat q / \hat y for omega in an interval
         return gain and phase list for all omega complex variables
         """
         for omg, u in zip(np.array(self.omega), self.u_table):
-            gain = self.A * omg ** 2 / (omg ** 2 - self.u ** 2)
+            gain = self.A * omg ** 2 / (omg ** 2 - u ** 2 + 1j * self.nu * self.epsilon * omg * u)
             self.gain.append(gain)
-            self.phase.append(np.angle(-gain, deg=False)/np.pi)
+            self.phase.append(np.angle(gain, deg=False) / np.pi)
 
     def solve_gain_u_k(self):
         """
@@ -123,9 +127,10 @@ class VIV(object):
         """
         self.set_u_k_table()
         for omg in (np.array(self.u_k_table)):
-            gain = self.A * omg ** 2 / (omg ** 2 - self.u_lim[0] ** 2 + 1j*self.nu*self.epsilon*omg*self.u_lim[0])
+            gain = self.A * omg ** 2 / (
+                        omg ** 2 - self.u_lim[0] ** 2 + 1j * self.nu * self.epsilon * omg * self.u_lim[0])
             self.gain.append(gain)
-            self.phase.append(np.angle(-gain, deg=False)/np.pi)
+            self.phase.append(np.angle(-gain, deg=False) / np.pi)
 
     # ***********************************************
     #  GETTERS
@@ -214,7 +219,7 @@ class VIV(object):
         return the array of the non dimensional wave number k
         """
         self.k_table = np.linspace(self.k_lim[0], self.k_lim[1], self.k_lim[2])
-    
+
     def set_u_k_table(self):
         """
         return the array of the non dimensional wave number for u and k fixed
@@ -241,7 +246,7 @@ class VIV(object):
 
     def set_polynomial_coefs(self, k, u):
         """
-        return the coefficient of the dispersion equation (determinant) 
+        return the coefficient of the dispersion equation (determinant)
         also found in self.coefs
         """
         if self.nu == 0:
@@ -254,12 +259,12 @@ class VIV(object):
             self.coefs = [1,
                           1j * self.nu * (-self.xi + (self.epsilon - self.beta) * u),
                           - k ** 2 - u ** 2 * (1 - self.A * self.M) + self.nu ** 2 * self.epsilon * (
-                                      self.beta * u ** 2 + self.xi * u),
+                                  self.beta * u ** 2 + self.xi * u),
                           1j * self.nu * (self.beta * u ** 3 - self.epsilon * k ** 2 * self.u + self.xi * u ** 2),
                           k ** 2 * u ** 2]
         return self.coefs
 
-    def discriminant(self,k):
+    def discriminant(self, k):
         """
         discriminant with nu = 0
         """
@@ -269,7 +274,7 @@ class VIV(object):
 
     def set_transfer_function(self, omega):
         """
-        G(omega) = | \hat q / \hat y |,  omega real  
+        G(omega) = | \hat q / \hat y |,  omega real
         """
         pass
         return abs(gain), np.angle(gain, deg=True)
@@ -279,10 +284,9 @@ class VIV(object):
         """
         omega = np.linspace(min_val, max_val, self.k_lim[2])
         pass
-        
 
     # ***********************************************
-    #  PLOTS 
+    #  PLOTS
     # ***********************************************
     def plot_u_omega_real(self):
         """Real part of omega for u
@@ -316,7 +320,7 @@ class VIV(object):
         plt.figure()
         plt.title(f"Modulus of $G$ for $\\nu={self.nu}$\nRivière & Doerfler", usetex=False)
         plt.plot(self.u_table, np.abs(self.gain), 'o')
-        plt.ylim([0,80])
+        plt.ylim([0, 80])
         plt.xlabel(r"$u$")
         plt.ylabel(r'$|G(\omega)|$')
         plt.grid()
